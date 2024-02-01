15304b7b8ed568d2aba91d557671823b0567df9e
Phoomparin Mano <phoomparin@gmail.com>
2024-02-01 18:13:09 +0700

    feat: setup basic canvas

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useRef} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLCanvasElement>()
+
+  function draw() {
+    const canvas = canvasRef.current
+    if (!canvas) return
+
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return
+
+    ctx.fillStyle = '#111'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  }
+
+  useEffect(() => {
+    draw()
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <canvas
+        className="w-screen h-screen"
+        ref={(element) => {
+          if (element) canvasRef.current = element
+        }}
+      />
+    </div>
   )
 }
 
