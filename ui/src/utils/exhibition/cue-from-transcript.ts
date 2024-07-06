f48ebfb66d8bc146d4409cd2280569217bdffd25
Phoomparin Mano <phoomparin@gmail.com>
2024-07-07 05:22:21 +0700

    transcript cues

--- ui/src/utils/exhibition/cue-from-transcript.ts
+++ ui/src/utils/exhibition/cue-from-transcript.ts
@@ -3,8 +3,11 @@ import {timecodeOf} from './timecode'
 import {AutomationCue} from '../../constants/exhibition-cues'
 import {GladiaTranscript} from '../../types/gladia-transcript'
 
-const SHORT_SENTENCE_WORD_COUNT = 5
-const FINAL_COUNTER = 10
+/** Exclude short sentences. */
+const SHORT_SENTENCE_WORDS = 6
+
+/** How long to wait until allowing next cue? */
+const CUE_WAIT_SECONDS = 20
 
 export async function loadTranscriptCue(): Promise<AutomationCue[]> {
   const body = await fetch('/transcription.json')
@@ -18,19 +21,23 @@ export function getCueFromTranscript(
 ): AutomationCue[] {
   const cues: AutomationCue[] = []
 
-  let counter = 0
+  let first_taken = false
+  let last_final_time = 0
 
   for (const u of transcript.transcription.utterances) {
     let final = false
 
-    if (u.words.length <= SHORT_SENTENCE_WORD_COUNT) {
-      // ignore short sentences
-    } else if (counter === 0) {
-      final = true
-      counter = FINAL_COUNTER
-    } else {
-      counter--
-      if (counter < 0) counter = 0
+    if (u.words.length > SHORT_SENTENCE_WORDS) {
+      const should_resume = u.start - last_final_time > CUE_WAIT_SECONDS
+
+      if (should_resume || !first_taken) {
+        final = true
+        last_final_time = u.start
+      }
+
+      if (!first_taken) {
+        first_taken = true
+      }
     }
 
     cues.push({
