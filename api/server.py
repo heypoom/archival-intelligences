d65d452879fd122de0da697bf826105429b26b30
Phoomparin Mano <poom@poom.dev>
2024-07-06 16:11:36 +0000

    fx

--- api/server.py
+++ api/server.py
@@ -29,6 +29,9 @@ async def websocket_endpoint(sock: WebSocket):
     await sock.accept()
 
     conn_id = handle_socket_connect(sock)
+
+    # this is blocking!
+    # pipelines cannot be interrupted at the moment, aside from disconnection
     send = create_send(sock)
 
     while True:
@@ -38,26 +41,28 @@ async def websocket_endpoint(sock: WebSocket):
 
             if command.startswith("P0:"):
                 prompt = strip(command, "P0")
-                send(infer_program_0(prompt, conn_id=conn_id))
+                await send(infer_program_0(prompt, conn_id=conn_id))
 
             elif command.startswith("P2:"):
                 strength = float(strip(command, "P2"))
-                send(infer_program_2(strength, conn_id=conn_id))
+                await send(infer_program_2(strength, conn_id=conn_id))
 
             elif command.startswith("P2B:"):
                 strength = float(strip(command, "P2B"))
-                send(infer_program_2_b(strength, conn_id=conn_id))
+                await send(infer_program_2_b(strength, conn_id=conn_id))
 
             elif command == "P3":
-                send(infer_program_3(" ", 5.5, conn_id=conn_id))
+                await send(infer_program_3(" ", 5.5, conn_id=conn_id))
 
             elif command.startswith("P3B:"):
                 prompt = strip(command, "P3B")
-                send(infer_program_3(f"{prompt}, photorealistic", 5.5, conn_id=conn_id))
+                await send(
+                    infer_program_3(f"{prompt}, photorealistic", 5.5, conn_id=conn_id)
+                )
 
             elif command.startswith("P4:"):
                 prompt = strip(command, "P4")
-                send(infer_program_4(prompt, conn_id=conn_id))
+                await send(infer_program_4(prompt, conn_id=conn_id))
 
             else:
                 await sock.send_text(f"unknown command: {command}")
