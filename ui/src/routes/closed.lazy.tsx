95356aaffdcb55adb1df2bc079caef5b952cc34b
Phoomparin Mano <phoomparin@gmail.com>
2024-07-07 17:16:28 +0700

    vf

--- ui/src/routes/closed.lazy.tsx
+++ ui/src/routes/closed.lazy.tsx
@@ -8,7 +8,7 @@ export function ClosedRoute() {
   return (
     <div className="flex flex-col items-center justify-center h-full font-mono min-h-screen bg-black text-white gap-y-8">
       <h1 className="text-4xl">Exhibition is now closed.</h1>
-      <h2 className="text-xl">Thanks for visiting! See you tomorrow.</h2>
+      <h2 className="text-xl">Thanks for visiting!</h2>
     </div>
   )
 }
