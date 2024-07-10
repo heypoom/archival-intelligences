fce6ccdab8a32685eb70b45a8de090ac330eb813
Phoomparin Mano <phoomparin@gmail.com>
2024-07-10 22:28:17 +0700

    focus ring

--- ui/src/components/SettingsButton.tsx
+++ ui/src/components/SettingsButton.tsx
@@ -1,11 +1,18 @@
-import {Link} from '@tanstack/react-router'
+import {Link, useMatchRoute, useNavigate} from '@tanstack/react-router'
+import {Icon} from '@iconify/react'
 
 export const SettingsButton = () => {
+  const navigate = useNavigate()
+  const mr = useMatchRoute()
+
+  if (mr({to: '/'})) return null
+
   return (
-    <Link to="/">
-      <button className="bg-[#2d2d30] text-white leading-3 px-[5px] py-[4px] rounded-md text-xs flex z-100000">
-        BACK
-      </button>
-    </Link>
+    <button
+      onClick={() => navigate({to: '/'})}
+      className="bg-[#2d2d30] text-white leading-3 p-[3px] w-5 h-5 rounded-full text-xs flex items-center justify-center z-100000 focus:outline-none focus:ring focus:ring-violet-300 focus:bg-violet-500 hover:bg-violet-500"
+    >
+      <Icon icon="lucide:settings" fontSize={18} />
+    </button>
   )
 }
