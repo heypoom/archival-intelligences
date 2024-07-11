98a8e9e10e45a5cb49129cbb843fce99f0df639f
Phoomparin Mano <phoomparin@gmail.com>
2024-07-12 04:29:50 +0700

    clear inference prev

--- ui/src/utils/exhibition/reset.ts
+++ ui/src/utils/exhibition/reset.ts
@@ -1,13 +1,14 @@
 import {$transcript} from '../../store/dictation'
 import {$guidance, DEFAULT_GUIDANCE} from '../../store/guidance'
 import {resetProgress} from '../../store/progress'
-import {$generating, $prompt} from '../../store/prompt'
+import {$generating, $inferencePreview, $prompt} from '../../store/prompt'
 import {$regenCount, disableRegen} from '../../store/regen'
 import {$programTimestamp, $videoTimestamp} from '../../store/timestamps'
 
 export function resetAll() {
   $prompt.set('')
   $transcript.set({transcript: '', final: false})
+  $inferencePreview.set('')
   $guidance.set(DEFAULT_GUIDANCE)
   $generating.set(false)
   $regenCount.set(0)
