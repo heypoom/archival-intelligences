afb21992146b4b9b0f9f67686288324f6544c348
Phoomparin Mano <poom@poom.dev>
2024-07-13 18:46:19 +0000

    two gpu

--- api/utils/pipelines.py
+++ api/utils/pipelines.py
@@ -3,19 +3,17 @@ import torch
 
 from diffusers import StableDiffusionImg2ImgPipeline, AutoPipelineForText2Image
 
-DEVICE = "cuda"
-
 start_time = time.time()
 
 text2img = AutoPipelineForText2Image.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
     torch_dtype=torch.float16,
-).to(DEVICE)
+).to("cuda:0")
 
 # Program 2 pipeline: Epic Poem of Malaya, Image to Image
 img2img = StableDiffusionImg2ImgPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5",
-).to(DEVICE)
+).to("cuda:1")
 
 
 print(f"two diffusion pipelines ready in {time.time() - start_time}s")
