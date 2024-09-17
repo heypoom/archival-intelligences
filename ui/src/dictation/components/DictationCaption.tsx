88c8a633c32b4f21f9cad195017ee8a49027d1d1
Phoomparin Mano <phoomparin@gmail.com>
2024-09-17 10:23:57 -0500

    change font

--- ui/src/dictation/components/DictationCaption.tsx
+++ ui/src/dictation/components/DictationCaption.tsx
@@ -2,9 +2,11 @@ import {useStore} from '@nanostores/react'
 import cn from 'classnames'
 
 import {$transcript} from '../../store/dictation'
+import {$exhibitionMode} from '../../store/exhibition'
 
 export const DictationCaption = () => {
   const {transcript, final} = useStore($transcript)
+  const isExhibition = useStore($exhibitionMode)
 
   if (!transcript) return null
 
@@ -12,9 +14,11 @@ export const DictationCaption = () => {
     <div className="text-center max-w-[1200px] px-2">
       <div
         className={cn(
-          'inline text-[28px] break-words bg-black py-2 px-6 font-extralight leading-[44px]',
+          'inline break-words bg-black py-2 px-6 font-extralight leading-[44px]',
           final && 'text-white',
-          !final && 'text-gray-300'
+          !final && 'text-gray-300',
+          isExhibition && 'text-[28px]',
+          !isExhibition && 'text-[42px]'
         )}
         style={{
           boxDecorationBreak: 'clone',
