b6f2a3ecb539090b03f9b63c252b084a4db5d345
Phoomparin Mano <phoomparin@gmail.com>
2024-09-23 09:42:55 -0500

    update video source

--- ui/src/constants/exhibition-videos.ts
+++ ui/src/constants/exhibition-videos.ts
@@ -1,8 +1,10 @@
+const videoSource =
+  'https://rui-an-fr-videos.poom.dev/rui-an-sep-23-fast-1080p.mp4'
+
 export const EXHIBITION_VIDEO_SOURCES = {
   // Main lecture video shown to the audience
-  lecture: 'https://rui-an-fr-videos.poom.dev/rui-an-sep-23-fast-1080p.mp4',
+  lecture: videoSource,
 
   // Emergency fallback video shown when the GPU server crashes
-  programFallback:
-    'https://rui-an-fr-videos.poom.dev/rui-an-sep-23-fast-1080p.mp4',
+  programFallback: videoSource,
 } as const
