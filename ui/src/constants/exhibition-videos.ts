00f8d90dee628c120cca0080fbe613d078a88521
Phoomparin Mano <phoomparin@gmail.com>
2024-09-24 04:59:33 -0500

    use handbrake version

--- ui/src/constants/exhibition-videos.ts
+++ ui/src/constants/exhibition-videos.ts
@@ -1,4 +1,4 @@
-const videoSource = 'https://rui-an-fr-videos.poom.dev/rui-an-sep-24-fast.mp4'
+const videoSource = 'https://rui-an-fr-videos.poom.dev/rui-an-sep-24-hb.mp4'
 
 export const EXHIBITION_VIDEO_SOURCES = {
   // Main lecture video shown to the audience
