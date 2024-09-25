3ac43328cb1abc95a0314e93bc868c194a03e1aa
Phoomparin Mano <phoomparin@gmail.com>
2024-09-25 08:34:34 -0500

    w

--- ui/src/routes/one.lazy.tsx
+++ ui/src/routes/one.lazy.tsx
@@ -12,7 +12,7 @@ function Index() {
     <div className="flex">
       <div className="fixed w-screen h-screen z-40">
         <div className="flex flex-col items-center justify-center w-full h-full">
-          <PromptInput className="min-w-[400px]" />
+          <PromptInput className="min-w-[600px]" />
 
           <div className="pt-4">
             <GuidanceSlider />
