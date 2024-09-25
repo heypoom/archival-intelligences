5562f8ab6a1b7d415d7e7a2cb3b3ec06c6f1c681
Phoomparin Mano <phoomparin@gmail.com>
2024-09-24 19:25:00 -0500

    change video fade logic

--- ui/src/components/FadeToBlack.tsx
+++ ui/src/components/FadeToBlack.tsx
@@ -2,21 +2,27 @@ import {useStore} from '@nanostores/react'
 
 import {$fadeStatus} from '../store/fader'
 import {useMatchRoute} from '@tanstack/react-router'
+import {useIsVideo} from '../hooks/useIsVideo'
+import {$exhibitionStatus} from '../store/exhibition'
 
 export const FadeToBlack = () => {
-  const visible = useStore($fadeStatus)
+  const fadeStatus = useStore($fadeStatus)
 
   const mr = useMatchRoute()
-  const cannotFadeToBlack = mr({to: '/'})
+  const isVideo = useIsVideo()
+  const cannotFadeToBlack = mr({to: '/'}) || mr({to: '/transcript-tester'})
+  const exhitionStatus = useStore($exhibitionStatus)
+
+  const isVisible = isVideo ? exhitionStatus.type !== 'active' : fadeStatus
 
   if (cannotFadeToBlack) return null
 
   return (
     <div
       className={`fixed z-[100000] w-full h-full top-0 left-0 inset-0 bg-black transition-opacity duration-[5s] ease-in-out ${
-        visible ? 'opacity-100' : 'opacity-0'
+        isVisible ? 'opacity-100' : 'opacity-0'
       }`}
-      style={{pointerEvents: visible ? 'auto' : 'none'}}
+      style={{pointerEvents: isVisible ? 'auto' : 'none'}}
     />
   )
 }
