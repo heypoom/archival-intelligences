3bdbaf285f5a4c7b5e06e4ff1370a724d1e71e1b
Phoomparin Mano <phoomparin@gmail.com>
2024-09-27 00:09:30 +0300

    program

--- ui/src/constants/exhibition-videos.ts
+++ ui/src/constants/exhibition-videos.ts
@@ -1,5 +1,5 @@
 export const EXHIBITION_VIDEO_SOURCES = {
   lecture: 'https://rui-an-fr-videos.poom.dev/rui-an-sep-24-fast-v2.mp4',
 
-  programVideo: 'https://rui-an-fr-videos.poom.dev/rui-an-sep-24-fast-v2.mp4',
+  programVideo: 'https://rui-an-fr-videos.poom.dev/rui-an-program.mp4',
 } as const
