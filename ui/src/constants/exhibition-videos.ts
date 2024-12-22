a0f058d44cedf6f5a16f5b5f3bae354da51721d3
Phoomparin Mano <phoomparin@gmail.com>
2024-12-22 16:03:23 +0700

    update video file

--- ui/src/constants/exhibition-videos.ts
+++ ui/src/constants/exhibition-videos.ts
@@ -1,5 +1,5 @@
 export const EXHIBITION_VIDEO_SOURCES = {
-  lecture: 'https://rui-an-fr-videos.poom.dev/rui-an-sep-24-fast-v2.mp4',
+  lecture: 'https://rui-an-fr-videos.poom.dev/rui-an-dec-22-fast.mp4',
 
-  programVideo: 'https://rui-an-fr-videos.poom.dev/rui-an-program.mp4',
+  programVideo: 'https://rui-an-fr-videos.poom.dev/rui-an-dec-22-program.mp4',
 } as const
