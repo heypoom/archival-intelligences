2aa9136ef10e45b5c2b5481e3e7d602b4a9b7fec
Phoomparin Mano <phoomparin@gmail.com>
2025-07-06 20:18:45 +0700

    cleanup env

--- requester/src/constants.ts
+++ requester/src/constants.ts
@@ -1,4 +1,4 @@
 export const MAX_VARIANT_COUNT = 30
-export const VALKEY_URL = 'valkey://raya.poom.dev'
+export const VALKEY_URL = process.env.VALKEY_URL
 export const PREGEN_VERSION_ID = 1
 export const PREGEN_UPLOAD_STATUS_KEY = `pregen/${PREGEN_VERSION_ID}/variant_upload_status`
