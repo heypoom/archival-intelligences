3c57ae218fb462a7f512b691937865f9a3795ebd
Phoomparin Mano <phoomparin@gmail.com>
2025-07-24 00:53:52 +0700

    foigoi-china.mp4

--- ui/src/constants/exhibition-videos.ts
+++ ui/src/constants/exhibition-videos.ts
@@ -1,5 +1,5 @@
 export const EXHIBITION_VIDEO_SOURCES = {
-  lecture: 'https://rui-an-fr-videos.poom.dev/rui-an-mar-02-2025-fast.mp4',
+  lecture: 'https://rui-an-fr-videos.poom.dev/foigoi-china.mp4',
 
   programVideo:
     'https://rui-an-fr-videos.poom.dev/rui-an-dec-22-program-v2.mp4',
