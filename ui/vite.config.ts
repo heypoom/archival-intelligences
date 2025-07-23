d5d08fafd79fb3834aeabb672613f5fa91fbad5d
Phoomparin Mano <phoomparin@gmail.com>
2025-07-24 00:29:57 +0700

    airgap vite.config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,4 +6,5 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  base: './',
 })
